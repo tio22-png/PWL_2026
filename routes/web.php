d263a8f547b4932603547db88e1b4473259be073
tio <hanggoronursetio@gmail.com>
2026-02-25 13:59:56 +0700

    Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,32 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 254107023006 | Nama: Hanggoro Nursetio Margono';
+});
+
+// Route Parameter - Optional Parameter (default: 'John')
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+// Route Parameter - Multiple Parameters
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . ' Komentar ke-: ' . $commentId;
+});
+
+// Route Parameter - Articles
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
 });
